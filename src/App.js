41d9f450b62ace8816aa50e2b6c9ca3e60ad9483
Jidan <106744009+jidandev@users.noreply.github.com>
2024-10-13 05:40:40 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ const App = () => {
           </div>
         )}
 
-        {action === 'login' && (
+        {action === 'login' && !token && (
           <div className="mb-4">
             <input
               type="text"
