27dab3cbdb32b9dd7097747b6100843244d19fbf
Jidan <106744009+jidandev@users.noreply.github.com>
2024-10-12 17:56:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './index.css';
@@ -11,10 +10,12 @@ const App = () => {
 
   useEffect(() => {
     socket.on('receiveMessages', (messages) => {
+      console.log('Messages received from server:', messages);
       setMessages(messages);
     });
 
     socket.on('receiveMessage', (message) => {
+      console.log('New message received from server:', message);
       setMessages((prevMessages) => [...prevMessages, message]);
     });
 
@@ -25,6 +26,7 @@ const App = () => {
   }, []);
 
   const sendMessage = () => {
+    console.log('Sending message:', message);
     socket.emit('sendMessage', message);
     setMessage('');
   };
@@ -35,7 +37,7 @@ const App = () => {
         <h2 className="text-2xl font-bold mb-4">Real-Time Chat</h2>
         <div className="mb-4 overflow-y-auto h-64 border rounded p-4 bg-gray-50">
           {messages.map((msg, index) => (
-            <div key={index} className="mb-2">{msg.content}</div>
+            <div key={index} className="mb-2">{msg.text}</div>
           ))}
         </div>
         <input
