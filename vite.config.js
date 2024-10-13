a1e3074b9f9d5838e8ab9ed2b7613987623c3c55
jidandev <jidangamer7@gmail.com>
2024-10-13 12:17:48 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ChatVersal',
+        short_name: 'ChatVers',
+        description: 'A simple real time chat',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
+});
